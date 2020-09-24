61ea494879ee70f09c46ffc0eaf77b34f6b3d759
Matthew McCall <matthew@mccall.app>
2020-09-23 20:43:13 -0400

    Added splash screen

---
+++ src/main/java/app/mccall/SplashScreen.java
@@ -0,0 +1,27 @@
+package app.mccall;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class SplashScreen extends JFrame {
+
+    private static final long serialVersionUID = -8539523818094681138L;
+
+    private ImageIcon splashImage;
+    private JLabel splashLabel;
+
+    public SplashScreen() {
+
+        splashImage = new ImageIcon("./splash.jpg");
+        splashLabel = new JLabel(splashImage);
+
+        add(splashLabel);
+
+        setResizable(false);
+        setUndecorated(true);
+        pack();
+        setLocationRelativeTo(null);
+
+    }
+}
