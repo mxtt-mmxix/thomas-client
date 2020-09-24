2dc99391a976af29795eec395f7a805e1ad94c54
Matthew McCall <matthew@mccall.app>
2020-09-23 20:43:41 -0400

    Refactored UI into separate class

--- src/main/java/app/mccall/Client.java
+++ src/main/java/app/mccall/Client.java
@@ -1,90 +1,57 @@
 package app.mccall;
 
-import java.awt.BorderLayout;
-import java.awt.event.*;
-
-import javax.swing.BorderFactory;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
 
-public class Client extends JFrame implements ActionListener {
-
-    private static final long serialVersionUID = 4976217117618960671L;
-    private String lastSender;
-
-    private JFrame window;
-    private JLabel titleLabel;
-    private JTextArea chatArea;
-    private JScrollPane scrollArea;
-    private JLabel inputLabel;
-    private JTextField inputField;
-    private JPanel chatPanel;
+public class Client {
 
     private Thomas thomas;
+    private SplashScreen splashScreen;
+    private ClientWindow window;
 
-    public Client() {
-
-        lastSender = "";
-
-        thomas = Thomas.getInstance();
-
-        window = new JFrame();
-        window.setTitle("project-thomas");
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        titleLabel = new JLabel("project-thomas");
-        titleLabel.setBorder(BorderFactory.createEmptyBorder(16, 16, 16, 16));
-
-        chatArea = new JTextArea();
-        chatArea.setEditable(false);
-        chatArea.setLineWrap(true);
-        chatArea.setRows(16);
-        chatArea.setWrapStyleWord(true);
-
-        scrollArea = new JScrollPane();
-        scrollArea.setViewportView(chatArea);
-        scrollArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-
-        inputLabel = new JLabel("Say something:");
-
-        inputField = new JTextField();
-        inputField.setColumns(32);
-        inputField.addActionListener(this);
-
-        chatPanel = new JPanel();
-        chatPanel.setBorder(BorderFactory.createEmptyBorder(16, 16, 16, 16));
-        chatPanel.add(inputLabel, BorderLayout.LINE_START);
-        chatPanel.add(inputField, BorderLayout.CENTER);
-
-        window.add(titleLabel, BorderLayout.PAGE_START);
-        window.add(scrollArea);
-        window.add(chatPanel, BorderLayout.PAGE_END);
-
-        window.pack();
-
-        window.setVisible(true);
-
-    }
-
-    private void sendMessage(String sender, String messsage) {
-
-        if (lastSender.equals(sender) || lastSender.equals(""))
-            chatArea.append(String.format("[%s] %s\n", sender, messsage));
-        else {
-            lastSender = sender;
-            chatArea.append(String.format("[%s] %s\n\n", sender, messsage));
+    private void fadeWindowIn(JFrame window) {
+        for (float opacity = 0; opacity < 1; opacity += 0.01)
+            window.setOpacity(opacity);
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        sendMessage("USER", inputField.getText());
-        thomas.log(inputField.getText());
-        inputField.setText("");
+    private void fadeWindowOut(JFrame window) {
+        for (float opacity = 1; opacity > 0; opacity -= 0.01)
+            window.setOpacity(opacity);
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Client() {
+
+        splashScreen = new SplashScreen();
+        splashScreen.setOpacity(0);
+        splashScreen.setVisible(true);
+
+        fadeWindowIn(splashScreen);
+
+        thomas = new Thomas();
+        thomas.initDB();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        window = new ClientWindow();
+
+        fadeWindowOut(splashScreen);
+        window.setVisible(true);
+
+        splashScreen.dispose();
+
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/app/mccall/ClientWindow.java
@@ -0,0 +1,91 @@
+package app.mccall;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.*;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class ClientWindow extends JFrame implements ActionListener {
+
+    private static final long serialVersionUID = -3762211696079735607L;
+
+    private JLabel titleLabel;
+    private JTextArea chatArea;
+    private JScrollPane scrollArea;
+    private JLabel inputLabel;
+    private JTextField inputField;
+    private JButton speakButton;
+    private JCheckBox ttsBox;
+    private JPanel chatPanel;
+
+    public ClientWindow() {
+
+        setTitle("project-thomas");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        titleLabel = new JLabel("project-thomas");
+        titleLabel.setBorder(BorderFactory.createEmptyBorder(16, 16, 16, 16));
+
+        chatArea = new JTextArea();
+        chatArea.setEditable(false);
+        chatArea.setLineWrap(true);
+        chatArea.setRows(16);
+        chatArea.setWrapStyleWord(true);
+
+        scrollArea = new JScrollPane();
+        scrollArea.setViewportView(chatArea);
+        scrollArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+        inputLabel = new JLabel("Say something:");
+
+        inputField = new JTextField();
+        inputField.setColumns(32);
+        inputField.addActionListener(this);
+
+        speakButton = new JButton();
+        speakButton.setText("Listen");
+        speakButton.addActionListener(this);
+
+        ttsBox = new JCheckBox();
+        ttsBox.setText("Speak");
+
+        chatPanel = new JPanel();
+        chatPanel.setLayout(new FlowLayout());
+        chatPanel.setBorder(BorderFactory.createEmptyBorder(16, 16, 16, 16));
+        chatPanel.add(inputLabel);
+        chatPanel.add(inputField);
+        chatPanel.add(speakButton);
+        chatPanel.add(ttsBox);
+
+        add(titleLabel, BorderLayout.PAGE_START);
+        add(scrollArea);
+        add(chatPanel, BorderLayout.PAGE_END);
+
+        pack();
+        setMinimumSize(new Dimension(getWidth(), getHeight()));
+        setLocationRelativeTo(null);
+
+    }
+
+    private void sendMessage(String sender, String messsage) {
+        chatArea.append(String.format("<%s> %s\n\n", sender, messsage));
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        inputField.setEnabled(false);
+        sendMessage("USER", inputField.getText());
+        inputField.setText("");
+        inputField.setEnabled(true);
+    }
+}
